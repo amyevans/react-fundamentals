3682b23be3eab558e84f3a6acd9040ee9c52da36
Amy Evans <amy@alley.co>
2021-07-01 17:38:40 -0400

    Exercise 6

--- src/exercise/06.tsx
+++ src/exercise/06.tsx
@@ -11,12 +11,12 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = <div className='box box--small' style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
+const mediumBox = <div className='box box--medium' style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
+const largeBox = <div className='box box--large' style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
 
 // 💰 the sizelssColorlessBox should still be a box, just with no size or color
-const sizelessColorlessBox = <div>sizeless colorless box</div>
+const sizelessColorlessBox = <div className='box' style={{fontStyle: 'italic'}}>sizeless colorless box</div>
 
 function App() {
   return (
